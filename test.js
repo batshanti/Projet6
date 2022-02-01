621c02b2bda7fd4b88aba54bbce12ddb28106c4b
LOUVET Teddy (Prestataire) <ex.teddy.louvet@dom-idf.justice.fr>
2022-02-01 14:39:55 +0100

    test fetch

--- test.js
+++ test.js
@@ -1,10 +1,15 @@
-async function fettt() {
-	console.log("aaaaaa");
+async function getSomething() {
 	let response = await fetch("http://localhost:8000/api/v1/titles?sort_by=-imdb_score");
-	console.log(response.text());
+	let data = await response.json();
+	let bestFilm = data.results[0];
+	let img = bestFilm['image_url'];
+	let myElement = document.getElementById('test');
+	/*console.log(bestFilm);*/
+	console.log(img);
+	myElement.style.backgroundImage = 'url(' + img + ')';
 }
 
-fettt();
+getSomething();
 
 
 /*let response = await fetch("http://localhost:8000/api/v1/titles");
