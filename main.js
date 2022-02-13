003943dace1029409208aedcbf194fc2fe705024
batshanti <teddylouvet@yahoo.fr>
2022-02-13 21:51:57 +0100

    minor css modification

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ fetch("http://localhost:8000/api/v1/titles?sort_by=-imdb_score").then(function(r
             document.getElementById('titreBestFilm').innerHTML = data.title
             document.getElementById('description').innerHTML = data.long_description
             let elementUrlPic = document.getElementById('aside-picture')
-            elementUrlPic.style.backgroundImage = 'url(' + img + ')'
+            elementUrlPic.src = img
+            /*elementUrlPic.style.backgroundImage = 'url(' + img + ')'*/
         })    
 })        
 
@@ -35,30 +36,7 @@ function getMovies (url, idName) {
 
 }
 
-
-
-/*fetch("http://localhost:8000/api/v1/titles?genre=thriller").then(function(res){
-    return res.json()
-}).then(function(data){
-    console.log("B")
-    data.results.forEach(function(oneMovie) {
-        new Movie(oneMovie,"bestFilmSection2")
-    })
-})
-
-fetch("http://localhost:8000/api/v1/titles/?genre=Action").then(function(res){
-    return res.json()
-}).then(function(data){
-    console.log("C")
-    data.results.forEach(function(oneMovie) {
-        new Movie(oneMovie,"bestFilmSection3")
-    })
-})*/
-
 getMovies("http://localhost:8000/api/v1/titles/?-imdb_score", "bestFilmImdb")
 getMovies("http://localhost:8000/api/v1/titles/?genre=Sci-Fi", "bestFilmSection1")
 getMovies("http://localhost:8000/api/v1/titles/?genre=thriller", "bestFilmSection2")
 getMovies("http://localhost:8000/api/v1/titles/?genre=Action", "bestFilmSection3")
-
-
-/*http://localhost:8000/api/v1/titles/?genre=Sci-Fi&page=2*/
